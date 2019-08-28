461b506ce6e0d366cda4d62da8990e0969aca1de
Jeffery <13533183756@163.com>
2019-08-29 01:26:14 +0800

    vue-router vuex code analysis

---
+++ vue/02_vue_router_vuex/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Router from 'vue-router'
+import Vuex from 'vuex'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
