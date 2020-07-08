4c7727edf32ad142f7a049b1f13ae4389e01a3ae
xie jeffery <xiejeffery@MacBook-Pro.lan>
2020-07-09 06:10:01 +0800

    vue源码

--- kkb-20/code/vue2-project/src/xvue/index.js
+++ kkb-20/code/vue2-project/src/xvue/index.js
@@ -10,14 +10,32 @@ class Xvue {
 
     // 响应式
     this.observe(this.$data)
-    
 
+
+  }
+  observe (obj) {
+    if (obj !== 'object' || obj == null) {
+      return
+    }
+    new Observer(obj)
   }
 
 
 }
+ class Observer {
+   constructor (value) {
+     this.value = value
+     this.walk(value) // 对象响应式
+   }
 
-const obj = {}
+   walk(obj) {
+     Object.keys(obj).forEach(key => {
+       defineReactive(obj, key, obj[key])
+     })
+   }
+ }
+
+
 
 function defineReactive(obj, key, val) {
   Object.defineProperty(obj, key, {
@@ -34,8 +52,6 @@ function defineReactive(obj, key, val) {
   })
 }
 
-defineReactive(obj, 'foo', 'foo')
-obj.foo = 'test foo'
 
 compileElement(node) {
   let nodeAttrs = node.attributes
