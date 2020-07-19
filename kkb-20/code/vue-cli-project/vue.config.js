ad314eb9581c3f41dc1f44eb3ccfa26753f92220
xie jeffery <xiejeffery@MacBook-Pro.lan>
2020-07-20 07:44:56 +0800

    项目最佳实践

---
+++ kkb-20/code/vue-cli-project/vue.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const port = 7070
+
+module.exports = {
+  //publicPath: '/best-plactice', // 部署应用包时的基本 URL
+  devServer: {
+    port
+  },
+  configureWebpack: {
+    name: "vue项⽬最佳实践",
+    resolve: {
+      alias: {
+        components: path.join(__dirname, '/src/component')
+      }
+    }
+  }
+}
---
+++ kkb-20/code/vue-ts-project/src/components/decor.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  // 装饰器工厂函数
+  function Component (options: any) {
+    return function (target: any) {
+      // 返回 Vue 配置对象
+      return Vue.extend(options)
+    }
+  }
+
+  @Component({
+    props: {
+      msg: {
+        type: String,
+        default: 'e33333'
+      }
+    }
+  })
+
+  export default class Dector extends Vue {
+
+  }
+  
+
+</script>
+
+<style scoped>
+
+</style>
